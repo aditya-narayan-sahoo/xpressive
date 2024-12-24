132b666e5f3a6f988a1a9f569d30b3a03dd2f3a4
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-12-25 00:02:00 +0530

    Add middleware for JSON parsing, URL-encoded data, and cookie parsing

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
-import express from "express";
 import dotenv from "dotenv";
+import express from "express";
+import cookieParser from "cookie-parser";
 import connectMongo from "./db/database.js";
 import authRoutes from "./routes/auth.routes.js";
 
@@ -7,6 +8,10 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5432;
+
+app.use(express.json({ limit: "7mb" }));
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
