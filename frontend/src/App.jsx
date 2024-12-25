7324faf98c10f0184eed6403a0b8234fdd3b4e14
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-12-25 23:22:22 +0530

    Add Side Panel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,11 @@ import { Routes, Route } from "react-router-dom";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
 import LoginPage from "./pages/auth/login/LoginPage";
 import HomePage from "./pages/home/HomePage";
+import Sidebar from "./components/common/Sidebar";
 const App = () => {
   return (
     <div className="flex max-w-6xl mx-auto">
+      <Sidebar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUpPage />} />
