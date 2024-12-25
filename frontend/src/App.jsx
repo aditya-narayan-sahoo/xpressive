cd361191cf1f12fcad56a92b1f1780d19cae3148
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-12-25 22:41:17 +0530

    Setup Routing & Folder Structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+import LoginPage from "./pages/auth/login/LoginPage";
+import HomePage from "./pages/home/HomePage";
 const App = () => {
   return (
-    <div className="text-3xl text-center font-extrabold">
-      Xpressive - Express your opinions on the platform X.
+    <div className="flex max-w-6xl mx-auto">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
     </div>
   );
 };
